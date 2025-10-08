55c7c10e581a9235ec0b54e70ca606b159f6857f
saynuk <saynuk@mac.com>
2025-10-08 10:56:57 -0400

    Update page.tsx

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { useState } from 'react';
-import { createBrowserClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createBrowserClient(
+const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
